d2e2ac45af38e63bb09ba709ffdc38c7b1d036a5
Markus Vetsch <markusvetsch@gmx.net>
2012-02-15 16:48:52 +0100

    updated script for DB init

--- PerformanceTracker/sql/init_database.sql
+++ PerformanceTracker/sql/init_database.sql
@@ -5,16 +5,21 @@
 * Date:		20120213
 */
 
--- Create schema do
+-- Create schema po
 
-CREATE SCHEMA "do"
+CREATE SCHEMA po
   AUTHORIZATION admin;
+  
+-- Grant schema access
+  
+GRANT USAGE ON SCHEMA po TO developer;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA po TO developer;
 
--- Create Table: "do".player
+-- Create Table: po.player
 
-CREATE TABLE "do".player
+CREATE TABLE po.player
 (
-  id bigint NOT NULL,
+  id SERIAL,
   firstname character varying(30) NOT NULL,
   lastname character varying(30) NOT NULL,
   birthdate date NOT NULL,
@@ -25,32 +30,21 @@ WITH (
   OIDS=FALSE,
   autovacuum_enabled=true
 );
-ALTER TABLE "do".player
+ALTER TABLE po.player
   OWNER TO admin;
 
--- Create Index: "do".idx_player_id_firstname_lastname
+-- Create Index: po.idx_player_id_firstname_lastname
 
 CREATE INDEX idx_player_id_firstname_lastname
-  ON "do".player
+  ON po.player
   USING btree
   (id , firstname COLLATE pg_catalog."default" , lastname COLLATE pg_catalog."default" );
-  
--- Create Sequence: "do".seq_player
-
-CREATE SEQUENCE "do".seq_player
-  INCREMENT 1
-  MINVALUE 1
-  MAXVALUE 9223372036854775807
-  START 1
-  CACHE 1;
-ALTER TABLE "do".seq_player
-  OWNER TO admin;  
 
 -- Create Table: "do".team
 
-CREATE TABLE "do".team
+CREATE TABLE po.team
 (
-  id bigint NOT NULL,
+  id SERIAL,
   descriptor character(4) NOT NULL,
   deleted boolean NOT NULL DEFAULT false,
   agegroup character(10),
@@ -60,30 +54,19 @@ WITH (
   OIDS=FALSE,
   autovacuum_enabled=true
 );
-ALTER TABLE "do".team
+ALTER TABLE po.team
   OWNER TO admin;
 
 -- Create Index: "do".idx_team_id_descriptorptor;
 
 CREATE UNIQUE INDEX idx_team_id_descriptor
-  ON "do".team
+  ON po.team
   USING btree
   (id , descriptor COLLATE pg_catalog."default" );
-
--- CREATE Sequence: "do".seq_team
-
-CREATE SEQUENCE "do".seq_team
-  INCREMENT 1
-  MINVALUE 1
-  MAXVALUE 9223372036854775807
-  START 1
-  CACHE 1;
-ALTER TABLE "do".seq_team
-  OWNER TO admin;
   
 -- Create Table: "do".perffeaturecategory
 
-CREATE TABLE "do".perffeaturecategory
+CREATE TABLE po.perffeaturecategory
 (
   id bigint NOT NULL,
   nicename character(30) NOT NULL,
@@ -93,39 +76,39 @@ WITH (
   OIDS=FALSE,
   autovacuum_enabled=true
 );
-ALTER TABLE "do".perffeaturecategory
+ALTER TABLE po.perffeaturecategory
   OWNER TO admin;
 
--- Create Index: "do".idx_perffeaturecategory_id_nicename
+-- Create Index: po.idx_perffeaturecategory_id_nicename
 
 CREATE UNIQUE INDEX idx_perffeaturecategory_id_nicename
-  ON "do".perffeaturecategory
+  ON po.perffeaturecategory
   USING btree
   (id , nicename COLLATE pg_catalog."default" );
   
--- Create Table: "do".perfsubfeaturecategory
+-- Create Table: po.perfsubfeaturecategory
 
-CREATE TABLE "do".perfsubfeaturecategory
+CREATE TABLE po.perfsubfeaturecategory
 (
   id bigint NOT NULL,
   parent_id integer NOT NULL,
   nicename character(30) NOT NULL,
   CONSTRAINT pk_perffeaturesubcategory PRIMARY KEY (id ),
   CONSTRAINT fk_perffeaturesubcategory_perffeaturecategory FOREIGN KEY (parent_id)
-      REFERENCES "do".perffeaturecategory (id) MATCH SIMPLE
+      REFERENCES po.perffeaturecategory (id) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE RESTRICT
 )
 WITH (
   OIDS=FALSE,
   autovacuum_enabled=true
 );
-ALTER TABLE "do".perfsubfeaturecategory
+ALTER TABLE po.perfsubfeaturecategory
   OWNER TO admin;
 
--- Create Index: "do".idx_perffeaturesubcategory_id
+-- Create Index: po.idx_perffeaturesubcategory_id
 
 CREATE INDEX idx_perffeaturesubcategory_id
-  ON "do".perfsubfeaturecategory
+  ON po.perfsubfeaturecategory
   USING btree
   (id );
 
@@ -134,20 +117,25 @@ CREATE INDEX idx_perffeaturesubcategory_id
   CREATE SCHEMA ref
   AUTHORIZATION admin;
   
+-- Grant schema access
+  
+GRANT USAGE ON SCHEMA ref TO developer;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA ref TO developer;
+  
  -- Create table ref.playerreference
   
   CREATE TABLE ref.playerreference
 (
-  id bigint NOT NULL,
+  id SERIAL,
   player_id bigint NOT NULL,
   team_id bigint NOT NULL,
   iscurrent boolean NOT NULL DEFAULT true,
   CONSTRAINT pk_playerreference PRIMARY KEY (id ),
   CONSTRAINT fk_player_playerreference FOREIGN KEY (player_id)
-      REFERENCES "do".player (id) MATCH SIMPLE
+      REFERENCES po.player (id) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE RESTRICT,
   CONSTRAINT fk_team_playerreference FOREIGN KEY (team_id)
-      REFERENCES "do".team (id) MATCH SIMPLE
+      REFERENCES po.team (id) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE RESTRICT
 )
 WITH (
@@ -164,22 +152,11 @@ CREATE UNIQUE INDEX idx_playerreference_id
   USING btree
   (id );
 
--- Create sequence ref.seq_playerreference
-  
-  CREATE SEQUENCE ref.seq_playerreference
-  INCREMENT 1
-  MINVALUE 1
-  MAXVALUE 9223372036854775807
-  START 1
-  CACHE 1;
-ALTER TABLE ref.seq_playerreference
-  OWNER TO admin;
-  
--- Create Table: "do".perfmeasurement
+-- Create Table: po.perfmeasurement
 
-CREATE TABLE "do".perfmeasurement
+CREATE TABLE po.perfmeasurement
 (
-  id bigint NOT NULL,
+  id SERIAL,
   playerreference_id bigint NOT NULL,
   value double precision,
   unit integer,
@@ -188,7 +165,7 @@ CREATE TABLE "do".perfmeasurement
   remark character varying(500),
   CONSTRAINT pk_perfmeasurement PRIMARY KEY (id ),
   CONSTRAINT fk_perfmeasurement_perfsubfeaturecategory FOREIGN KEY (perfsubfeature_id)
-      REFERENCES "do".perfsubfeaturecategory (id) MATCH SIMPLE
+      REFERENCES po.perfsubfeaturecategory (id) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE RESTRICT,
   CONSTRAINT fk_perfmeasurement_playerreference FOREIGN KEY (playerreference_id)
       REFERENCES ref.playerreference (id) MATCH SIMPLE
@@ -198,51 +175,45 @@ WITH (
   OIDS=FALSE,
   autovacuum_enabled=true
 );
-ALTER TABLE "do".perfmeasurement
+ALTER TABLE po.perfmeasurement
   OWNER TO admin;
 
--- Create Index: "do".fkidx_perfmeasurement_perfsubfeaturecategory
+-- Create Index: po.fkidx_perfmeasurement_perfsubfeaturecategory
 
 CREATE INDEX fkidx_perfmeasurement_perfsubfeaturecategory
-  ON "do".perfmeasurement
+  ON po.perfmeasurement
   USING btree
   (perfsubfeature_id );
 
--- Create Index: "do".fkidx_perfmeasurement_playerref
+-- Create Index: po.fkidx_perfmeasurement_playerref
 
 CREATE INDEX fkidx_perfmeasurement_playerref
-  ON "do".perfmeasurement
+  ON po.perfmeasurement
   USING btree
   (playerreference_id );
 
--- Create Index: "do".idx_perfmeasurement_id_playerreferenceid
+-- Create Index: po.idx_perfmeasurement_id_playerreferenceid
 
 CREATE INDEX idx_perfmeasurement_id_playerreferenceid
-  ON "do".perfmeasurement
+  ON po.perfmeasurement
   USING btree
   (id , playerreference_id );
 
--- Create sequence "do".seq_perfmeasurement
-  
-CREATE SEQUENCE "do".seq_perfmeasurement
-  INCREMENT 1
-  MINVALUE 1
-  MAXVALUE 9223372036854775807
-  START 1
-  CACHE 1;
-ALTER TABLE "do".seq_perfmeasurement
-  OWNER TO admin;
-  
 -- Create schema mt
   
 CREATE SCHEMA mt
   AUTHORIZATION admin;
+
+-- Grant access
+  
+GRANT USAGE ON SCHEMA mt TO developer;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA mt TO developer;
   
 -- Create Table: mt.playerdatahistory
 
 CREATE TABLE mt.playerdatahistory
 (
-  id bigint NOT NULL,
+  id SERIAL,
   player_id integer NOT NULL,
   weight double precision,
   height integer,
@@ -250,7 +221,7 @@ CREATE TABLE mt.playerdatahistory
   "timestamp" timestamp without time zone NOT NULL,
   CONSTRAINT pk_playerdata PRIMARY KEY (id ),
   CONSTRAINT fk_player_id FOREIGN KEY (player_id)
-      REFERENCES "do".player (id) MATCH SIMPLE
+      REFERENCES po.player (id) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE NO ACTION
 )
 WITH (
@@ -266,14 +237,3 @@ CREATE INDEX idx_playerdatahistory_id_playerid
   ON mt.playerdatahistory
   USING btree
   (id , player_id );
-
--- Create sequence mt.seq_playerdatahistory
-  
-  CREATE SEQUENCE mt.seq_playerdatahistory
-  INCREMENT 1
-  MINVALUE 1
-  MAXVALUE 9223372036854775807
-  START 1
-  CACHE 1;
-ALTER TABLE mt.seq_playerdatahistory
-  OWNER TO admin;
